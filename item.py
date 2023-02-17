7d1650aa2415ce1a24eded19204c4a6d088b1bc2
Jay <jay.pena1224@proton.me>
2023-02-17 13:34:54 -0500

    Create classes for items

--- item.py
+++ item.py
@@ -1,25 +1,19 @@
-class item:
-    def __init__(self, name, price, stock, desc):
+class Item:
+    def __init__(self, name, price, stock, cond):
         self.name = name
         self.price = price
         self.stock = stock
-        self.desc = desc
+        self.condition = cond
     def showItem(self):
         print(f"Your created item: {self.name}, ${self.price}, {self.stock} count, {self.desc}" )
 
-name = None
-while not name: 
-    name = input("Put the name of the item: ")
-price = None
-while not price:
-    price = input("Type the price of one item: ")
-stock = None
-while not stock:
-    stock = input("How many do you currently have: ")
-desc = None
-while not desc:
-    desc = input("Provide a short description: ")
+class Clothing(Item):
+    def __init__(self, name, price, stock, cond, size):
+        super().__init__(name, price, cond, stock)
+        self.size = size
 
-item1 = item(name, price, stock, desc)
-
-item1.showItem()
+class Figure(Item):
+    def __init__(self, name, price, stock, scale, manu, cond):
+        super().__init__(name, price, cond, stock)
+        self.scale = scale
+        self.manu = manu
